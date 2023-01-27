9be516f5b21d19ec9e501d91c12ef5c46b9225df
sahibibadov <117587344+sahibibadov@users.noreply.github.com>
2023-01-27 20:05:08 +0400

    prduct doldurulmasi

--- main.js
+++ main.js
@@ -41,13 +41,13 @@ document.body.addEventListener('click', (event) => {
 
 // backdropa klikde acilan modalllarin baglanmasi
 const backdrop = document.querySelector('.backdrop');
-/* shopun acilan menyusun kenara klikde baglnmasi */
+/* backdropa klikde baglanmasi shop ve modalin */
 const listener = backdrop.addEventListener('click', (e) => {
     e.stopPropagation();
     if (e.target == backdrop) {
-        backdrop.classList.remove('open');
         shopMenu.classList.remove('open');
         searchModal.classList.remove('open');
+        backdrop.classList.remove('open');
 
         backdrop.removeEventListener('click', listener);
     };
@@ -93,8 +93,6 @@ closeBtn.addEventListener('click', () => {
 
 
 
-
-
 /* user clikde acilan menyu */
 const user = document.querySelector('.user-li');
 const accMenyu = document.querySelector('.account-dropmenu');
@@ -106,3 +104,133 @@ user.addEventListener('click', () => {
 
 
 
+/* =================================== */
+
+// productlari elave etmek
+
+const products = [
+    {
+        id: 1,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '10%',
+        title: 'PS5 controller',
+        price: '$29.99',
+        oldPrice: '$49.99'
+    },
+    {
+        id: 2,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$1099.99',
+        oldPrice: '$1249.99'
+    },
+    {
+        id: 3,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '10%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+    {
+        id: 4,
+        images: ['image/product-01.png', 'image/product-011.png'],
+        badget: '20%',
+        title: 'Galaxy Z Fold ',
+        price: '$2099.99',
+        oldPrice: '$2349.99'
+    },
+];
+
+
+// produclari html yazmaq
+const productsList = document.querySelector('.products');
+
+products.forEach(({ id, images, badget, title, price, oldPrice }) => {
+    productsList.innerHTML += `
+                    <div class="product" daya-id="${id}">
+                        <div class="thumblain">
+                            <a href="#">
+                                <img src="${images[0]}" alt="1ci ps">
+                                <img class="hover-image" src="${images[1]}" alt="2ci ps">
+                            </a>
+                            <div class="product-badget">${badget} off</div>
+
+                            <div class="prod-hover-action">
+                                <ul>
+                                    <li><a class="action-btn" href="#"><i class="fa-regular fa-eye"></i></a></li>
+                                    <li><a class="add-cart" href="#">Add to cart</a></li>
+                                    <li><a class="action-btn" href="#"><i class="fa-regular fa-heart"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="product-content">
+                            <div class="prod-raiting">
+                                <span>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                </span>
+                                <span>(64)</span>
+                            </div>
+                            <h5 class="prod-title"><a href="#">${title}</a></h5>
+                            <div class="prod-price">
+                                <span>${price}</span>
+                                <span class="old-price">${oldPrice}</span>
+                            </div>
+                        </div>
+
+                    </div>
+    `
+});
